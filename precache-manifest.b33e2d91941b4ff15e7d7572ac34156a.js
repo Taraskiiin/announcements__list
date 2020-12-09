120fa5a99c1a961ae8eb9d0e6c5a8837cec1fe02
Taraskiiin <taraskravets95@gmail.com>
2020-12-09 22:43:18 +0200

    Updates

---
+++ precache-manifest.b33e2d91941b4ff15e7d7572ac34156a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c152a7814d4e8826c77df32ab8812fd5",
+    "url": "/announcements__list/index.html"
+  },
+  {
+    "revision": "1cbb887c6057b2d560ac",
+    "url": "/announcements__list/static/css/main.bb5c2281.chunk.css"
+  },
+  {
+    "revision": "d2e60c0e907b1dece66f",
+    "url": "/announcements__list/static/js/2.c5a0b603.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/announcements__list/static/js/2.c5a0b603.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cbb887c6057b2d560ac",
+    "url": "/announcements__list/static/js/main.9596b025.chunk.js"
+  },
+  {
+    "revision": "c3e7d731b4a2f6f224ee",
+    "url": "/announcements__list/static/js/runtime-main.302a7348.js"
+  }
+]);
