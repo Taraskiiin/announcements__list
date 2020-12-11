7662b7841bad73ddf4b069c7f0e3a63e77483ba4
taraskiiin <taraskravets95@gmail.com>
2020-12-11 17:02:52 +0200

    create text- block

--- src/components/AppContent/TextBlock.js
+++ src/components/AppContent/TextBlock.js
@@ -1,49 +1,23 @@
-import React, { useState, useRef } from "react";
-import { changeTextOfAnnouncement } from "../../helpers/api";
+import React, { useState, Fragment } from "react";
+
 import "./TextBlock.css";
-
+import "./EditTextBlock";
 const TextBlock = ({ list }) => {
-  const [isInEditMode, setInEditMode] = useState(true);
-  const ChosenId = list._id;
-  const theInputText = useRef();
-
-  const renderEditView = () => {
-    return (
-      <div className="announcements__text">
-        <input defaultValue={list.post} ref={theInputText}></input>
-        <button className="update__text" onClick={() => updatingText()}>
-          <i className="fa fa-save icon"></i>
-        </button>
-        <button
-          className="close__texteditor"
-          onClick={() => setInEditMode(!isInEditMode)}
-        >
-          <i className="fa fa-close icon"></i>
-        </button>
-      </div>
-    );
-  };
-  const renderDefaultView = () => {
-    return (
-      <div
-        onDoubleClick={() => setInEditMode(!isInEditMode)}
-        className="announcements__text"
-      >
-        {list.post}
-      </div>
-    );
-  };
-  const updatingText = () => {
-    setInEditMode(!isInEditMode);
-    changeTextOfAnnouncement(ChosenId, theInputText).catch(() =>
-      alert(`We can't change this text`)
-    );
-  };
+  const [isInEditMode, setInEditMode] = useState(false);
   return (
-    <div>
-      {isInEditMode ? renderEditView() : renderDefaultView()}
+    <Fragment>
+      {isInEditMode ? (
+        <EditTextBlock list={list} setInEditMode={setInEditMode()}/>
+      ) : (
+        <div
+          onDoubleClick={() => setInEditMode(!isInEditMode)}
+          className="announcements__text"
+        >
+          {list.post}
+        </div>
+      )}
       {list && <p className="announcements__time">{list.time}</p>}
-    </div>
+    </Fragment>
   );
 };
 export default TextBlock;
